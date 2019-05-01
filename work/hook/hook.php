99735919418e273ede1adfd05e1958f3f166a230
Yvan Burrie <yvan.burrie@my.jcu.edu.au>
2019-05-01 17:53:12 +1000

    Force MySQL dump from hook script

--- work/hook/hook.php
+++ work/hook/hook.php
@@ -1,5 +1,11 @@
+<pre>
 <?php
 
 var_dump($_POST);
-
 file_put_contents('hook.txt', $_POST);
+
+system('mysqldump staging --password=x --user=cp3402 --single-transaction >/var/backups/base_dump.sql', $output);
+var_dump($output);
+
+?>
+</pre>
