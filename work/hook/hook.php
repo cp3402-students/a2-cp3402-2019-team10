782dcfeaf4305431e3354438b086809c6f8a9ea2
Yvan Burrie <yvan.burrie@my.jcu.edu.au>
2019-05-03 00:28:35 +1000

    Test hook 4

--- work/hook/hook.php
+++ work/hook/hook.php
@@ -56,7 +56,7 @@ $REPOSITORIES.add(new Repository(
     'Q29mZmVlQ2FuLjM0MDI=',
     '/var/www/html/staging/base_dump'));
 
-$LOG = "\n" . str_val($_POST);
+$LOG = "\n" . str_val($_POST) . "\n";
 
 $repository = &$_POST['repository'];
 if( is_array($repository) ){
