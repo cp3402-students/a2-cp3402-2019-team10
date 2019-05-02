7b5ac529bd04a7e6e20f61a1e547426b00bdf1dc
Yvan Burrie <yvan.burrie@my.jcu.edu.au>
2019-05-02 22:15:46 +1000

    Add Repository class to hook

--- work/hook/hook.php
+++ work/hook/hook.php
@@ -1,6 +1,27 @@
 <pre>
 <?php
 
+class Repository {
+
+    public $id = 0;
+    public $name = NULL;
+    public $path = NULL;
+
+    public function __construct($id, $name, $path) {
+        $this->id   = $id;
+        $this->name = $name;
+        $this->path = $path;
+    }
+
+    public function pull() {
+        if( $this->path !== NULL ){
+            system('cd ' . $this->path);
+        }
+        system('git pull > git-pull.out 2> git-pull.err');
+    }
+}
+
+
 var_dump($_POST);
 file_put_contents('hook.txt', $_POST);
 
