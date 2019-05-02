d3215c33f6ca108daa836a3ddda583f87a63cb04
Yvan Burrie <yvan.burrie@my.jcu.edu.au>
2019-05-02 23:55:15 +1000

    Do minor fixes to hook and simplify it

--- work/hook/hook.php
+++ work/hook/hook.php
@@ -10,8 +10,9 @@ class Repository {
     public $db_name = NULL;
     public $db_user = NULL;
     public $db_pass = NULL;
+    public $db_dump = NULL;
 
-    public function __construct($id, $name, $path, $base, $db_name, $db_user, $db_pass) {
+    public function __construct($id, $name, $path, $base, $db_name, $db_user, $db_pass, $db_dump) {
         $this->id   = $id;
         $this->name = $name;
         $this->path = $path;
@@ -19,17 +20,24 @@ class Repository {
         $this->db_name = $db_name;
         $this->db_user = $db_user;
         $this->db_pass = $db_pass;
+        $this->db_dump = $db_dump;
     }
 
     public function pull() {
+        /* Redirect to repo path: */
         if( $this->path !== NULL ){
             system('cd ' . $this->path);
         }
+        /* Run pull command: */
         system('git pull > git-pull.out 2> git-pull.err');
     }
 
     public function pullSql() {
-        system('mysqldump ' . $this->db_name . ' --password=' . base64_decode($this->db_pass) . ' --user=' . $this->db_user . ' --single-transaction >/var/www/html/staging/base_dump.sql 2> /var/www/html/staging/base_dump.err', $output);
+        /* Export old database: */
+        system('mysqldump ' . $this->db_name . ' --password=' . base64_decode($this->db_pass) . ' --user=' . $this->db_user . ' --single-transaction >' . $this->db_dump . '.sql 2> ' . $this->db_dump . '.err', $output);
+        return;
+        /* Import new database: */
+        system('mysql -u ' . $this->db_user . ' -p ' . base64_decode($this->db_pass) . ' ' . $this->db_name . ' < ' . $this->db_dump . '.sql', $output);
     }
 }
 
@@ -40,36 +48,37 @@ $REPOSITORIES.add(new Repository(
     '/var/www/html/staging/wp-content',
     'dbdump.sql',
     'staging',
+    'cp3402',
     'Q29mZmVlQ2FuLjM0MDI=',
-    'CoffeeCan.3402'));
+    '/var/www/html/staging/base_dump'));
 
-$post_repository = &$_POST['repository'];
-if( is_array($post_repository) ){
-    $post_repository_id   = &$post_repository['id'];
-    $post_repository_name = &$post_repository['name'];
-    if( is_integer($post_repository_id) && is_string($post_repository_name) ){
+$repository = &$_POST['repository'];
+if( is_array($repository) ){
+    $repository_id   = &$repository['id'];
+    $repository_name = &$repository['name'];
+    if( is_integer($repository_id) && is_string($repository_name) ){
         foreach( $REPOSITORIES as $repo ){
             /* Verify whether the request matches any repos: */
-            if( $repo->id != $post_repository_id || $repo->name != $post_repository_name ) continue;
+            if( $repo->id != $repository_id || $repo->name != $repository_name ) continue;
             /* Pull repo upon push request: */
-            $post_repository_pusher = &$post_repository['pusher'];
-            if( is_array($post_repository_pusher) ){
+            $pusher = &$_POST['pusher'];
+            if( is_array($pusher) ){
                 $repo->pull();
             }
             $base_found = false;
             /* Iterate all committed files: */
-            $post_repository_commits = &$post_repository['commits'];
-            if( is_array($post_repository_commits) ){
-                foreach( $post_repository_commits as $post_repository_commit ){
-                    if( !is_array($post_repository_commit) ) continue;
-                    $post_repository_commit_files = &$post_repository_commit['modified'];
-                    if( !is_array($post_repository_commit_files) ){
-                        $post_repository_commit_files = &$post_repository_commit['added'];
+            $commits = &$_POST['commits'];
+            if( is_array($commits) ){
+                foreach( $commits as $commit ){
+                    if( !is_array($commit) ) continue;
+                    $commit_files = &$commit['modified'];
+                    if( !is_array($commit_files) ){
+                        $commit_files = &$commit['added'];
                     }
-                    foreach( $post_repository_commit_files as $post_repository_commit_file ){
-                        if( !is_string($post_repository_commit_file) ) continue;
+                    foreach( $commit_files as $commit_file ){
+                        if( !is_string($commit_file) ) continue;
                         /* Check if the file is the SQL backup file: */
-                        if( !$base_found && $repo->base == $post_repository_commit_file ){
+                        if( !$base_found && $repo->base == $commit_file ){
                             $base_found = true;
                             $repo->pullSql();
                         }
