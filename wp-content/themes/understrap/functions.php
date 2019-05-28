433dca1d6f44520d701801de4d16ccf9e5e772af
Yvan Burrie <yvan.burrie@my.jcu.edu.au>
2019-05-28 16:16:14 +1000

    Enable plugin activation for theme

--- wp-content/themes/understrap/functions.php
+++ wp-content/themes/understrap/functions.php
@@ -25,6 +25,7 @@ $understrap_includes = array(
 	'/woocommerce.php',                     // Load WooCommerce functions.
 	'/editor.php',                          // Load Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
+	'/plugin-activator.php',
 );
 
 foreach ( $understrap_includes as $file ) {
